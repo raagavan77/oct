0b74c40ac1920db70ab496fea9bd866370c337c1
rv <rv@gmail.com>
2024-10-01 15:35:12 +0000

    modified in branch

--- age.sh
+++ age.sh
@@ -6,7 +6,10 @@ echo "child"
 elif [ $age -ge 13 -a $age -le 19 ];
 then
 echo "tennger"
-elif [ $age -ge 20 ];
+elif [ $age -ge 20 -a $age -lt 60 ];
 then
 echo "adult"
+elif [ $age -ge 60 ];
+then
+echo "oldage"
 fi
